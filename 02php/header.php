7830ff488e68fab83c391966c08daf41f55af223
lindsaymoon <75105280+lindsaymoon@users.noreply.github.com>
2021-01-15 14:38:21 +0900

    today

---
+++ 02php/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>나의홈피</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header id="header">
+		<h1><a href="./">logo</a></h1>
+		<nav class="gnb">
+			<ul class="clearfix">
+				<li><a href="company.html">company</a></li>
+				<li><a href="location.html">location</a></li>
+				<li><a href="#">menu</a></li>
+				<li><a href="#">menu</a></li>
+				<li><a href="#">menu</a></li>
+			</ul>
+		</nav>
+	</header>
